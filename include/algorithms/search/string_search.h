6387696788cc9d6330c6b7827bb6566587cb9d50
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2023-09-23 21:23:05 -0600

    strinh_search.h fix

--- include/algorithms/search/string_search.h
+++ include/algorithms/search/string_search.h
@@ -3,35 +3,47 @@
 template <typename T>
 class StringSearch{
 private:
-    T* Text;
+    T Text;
     size_t size;
 
 public:
-    StringSearch(T* Text, size_t size);
-    bool KMP(T* word, size_t word_size);
+    StringSearch(T Text, size_t size);
+    int KMP(T word, size_t word_size);
+    int BM(T word, size_t word_size);
 };
 
 template <typename T>
-StringSearch<T>::StringSearch(T* Text, size_t size){
+StringSearch<T>::StringSearch(T Text, size_t size){
     this->Text = Text;
     this->size = size;
 }
 
 template <typename T>
-bool StringSearch<T>::KMP(T* word, size_t word_size){
+int StringSearch<T>::KMP(T word, size_t word_size){
+  int position = 0;
   size_t count = 0;
 
   for(size_t i = 0; i <= size; i++){
     if(count == word_size){
-      return true;
+      return position;
     }
     if(word[count] == Text[i]){
+      if(position == 0){
+        position = i;
+      }
       count++;
     }
     else{
+      position = 0;
       count = 0;
     }
   }
 
-  return false;
+  std::cout << "La palabra '" << word << "' no se encuentra en el texto." << std::endl;
+  return -1;
+}
+
+template <typename T>
+int StringSearch<T>::BM(T word, size_t word_size){
+
 }

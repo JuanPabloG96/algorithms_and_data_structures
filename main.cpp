730b85f2b89ed48c293877ec6fc64483baab264d
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2023-09-25 23:03:04 -0600

    back to normal main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,21 @@
-#include <iostream>
 #include <string>
 #include <cstring>
 #include <chrono>
 
 #include "include/algorithms/search/string_search.h"
+#include "include/data_structures/stack.h"
 
 using namespace std;
 
-int main(){
+int main()
+{
   auto start_time = std::chrono::high_resolution_clock::now();
-  //Test area
+  // Test area
 
-  string text = "akdonqwoihcaksndw";
-  string word = "ihca";
-  size_t size = text.length();
-  size_t word_size = word.length();
-
-  StringSearch<string> element(text, size);
-
-  if(element.KMP(word, word_size) != -1){
-    cout << "La palabra '" << word << "' esta en la posicion: " << element.KMP(word, word_size) << endl;
-  }
-
-  //Test area
+  // Test area
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-  std::cout << "Tiempo de ejecución: " << duration.count() << " micro-segundos" << std::endl;
+  std::cout << "\nCode run time: " << duration.count() << " microseconds" << std::endl;
 
   return 0;
 }

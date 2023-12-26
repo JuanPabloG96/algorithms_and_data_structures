c6824ddaef214a6829007a57e9fda501b80f7ae1
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2023-12-25 18:36:57 -0700

    refresh

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <string>
-#include <cstring>
 #include <chrono>
 
+#include "include/algorithms/search/array_search.h"
 #include "include/algorithms/search/string_search.h"
 #include "include/data_structures/stack.h"
+#include "include/data_structures/node_class/double_node.h"
+#include "include/data_structures/double_link_list.h"
 
 using namespace std;
 
@@ -12,7 +14,7 @@ int main()
   auto start_time = std::chrono::high_resolution_clock::now();
   // Test area
 
-  // Test area
+    // Test area
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
   std::cout << "\nCode run time: " << duration.count() << " microseconds" << std::endl;
